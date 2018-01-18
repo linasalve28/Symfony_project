6fb7500454340cbc37b8d85634dab67b4fed3663
alinechribeiro <alinechribeiro@gmail.com>
2018-01-18 15:56:54 +0000

    branch-5-aline login.php changed for testing conflicts

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
 echo "This is login page" ;
-echo "This is a change for testing branches and commits from local to remote repository."
-echo "testing branches conflicts."
+echo "This is a change for testing branches and commits from local to remote repository.";
+echo "branch-5-aline:testing branches conflicts and more conflicts around here.";
 ?>
